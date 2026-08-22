3fe70dde2c17a00231ccd44bb5eda614ecbcb9a6
adhfmz7 <ahmadaldasouqi559@gmail.com>
2026-08-21 22:20:41 -0700

    feat(tools): add toolcall model

--- src/tools.py
+++ src/tools.py
@@ -1,7 +1,7 @@
 # tools.py
 # registry of tools agent can access
 
-from typing import Any, Callable
+from typing import Any, Callable, Mapping
 from dataclasses import dataclass
 
 '''
@@ -19,6 +19,11 @@ class Tool:
 
     def __call__(self, **kwargs: Any) -> Any:
         return self.function(**kwargs)
+
+@dataclass 
+class ToolCall:
+    name: str
+    arguments: Mapping[str, Any]
 
 class ToolRegistry:
 
@@ -41,8 +46,11 @@ class ToolRegistry:
         self.tools[tool.name] = tool
         return function
 
-    # def get_tools(self, api_format: str = 'ollama') -> list[Callable]:
-    #     return self.tools.keys()
+    def get_tools(self, provider: str): #Figure out type hints
+        if provider == 'ollama':
+            return [tool.function for tool in self.tools.values()]
+        else:
+            print("Invalid provider")
 
 
 registry = ToolRegistry()
@@ -50,4 +58,5 @@ registry = ToolRegistry()
 @registry.register
 def add(a: int, b: int) -> int:
     '''takes adds two integers a and b and returns their sum'''
+    print("Invoking tool add")
     return a + b
